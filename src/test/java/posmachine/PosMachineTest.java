0a17fa46ae2dc4c93153ddd47f2214a19bad6dda
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-14 20:06:33 +0800

    Completed Test

--- src/test/java/posmachine/PosMachineTest.java
+++ src/test/java/posmachine/PosMachineTest.java
@@ -1,7 +1,11 @@
 package posmachine;
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PosMachineTest {
 
@@ -9,22 +13,43 @@ public class PosMachineTest {
     @Test
     public void should_get_receipt_using_real_price_calculator() {
         //given
+        PriceCalculator calculator = new PriceCalculator();
+        PosMachine posMachine = new PosMachine(calculator);
         //when
+
         //then
+        Assertions.assertThrows(UnsupportedOperationException.class, () ->
+                posMachine.getReceipt("Coke"));
     }
 
     @Test
-    public void should_get_receipt_using_sub_price_calculator() {
+    public void should_get_receipt_using_stub_price_calculator() {
         //given
+        PriceCalculator calculator = new StubPriceCalculator();
+        PosMachine posMachine = new PosMachine(calculator);
         //when
+        String receipt = posMachine.getReceipt("Coke");
         //then
+        Assertions.assertEquals("Name: Coke, Price: 10.0", receipt);
     }
 
     @Test
     public void should_get_receipt_using_real_price_calculator_with_mockito() {
         //given
+        PriceCalculator calculator = mock(PriceCalculator.class);
+        PosMachine posMachine = new PosMachine(calculator);
         //when
+        when(calculator.calculate(anyString())).thenReturn(10.0);
+
+        String receipt = posMachine.getReceipt("Cola");
         //then
+        Assertions.assertEquals("Name: Cola, Price: 10.0", receipt);
     }
 
+    private class StubPriceCalculator extends PriceCalculator {
+        @Override
+        public double calculate(String productName) {
+            return 10.0;
+        }
+    }
 }
